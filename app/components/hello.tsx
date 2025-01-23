589c9f338bfe8eb9a6aa3b27cb26a50911a13a19
ujsquared <ujsquared@gmail.com>
2025-01-24 02:28:17 +0530

    restart

---
+++ app/components/hello.tsx
@@ -0,0 +1,10 @@
+"use client"
+function Hello(){
+    console.log("Hello I am a client component")
+    return (
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    );
+}
+export default Hello;
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Hello from "@/app/components/hello";
+
+export default function Home() {
+  return (
+      <>
+          <div>Hello World!</div>
+          <Hello />
+      </>
+  );
+}
