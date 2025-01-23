b0ee417ba4dc8c26d64c43b17b1187b0c6768b18
ujsquared <ujsquared@gmail.com>
2025-01-24 05:21:59 +0530

    remove dashboard

--- components/AuthButtons.tsx
+++ components/AuthButtons.tsx
@@ -5,7 +5,7 @@ import { signIn } from "next-auth/react";
 export function LoginButton() {
   return (
     <button
-      onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
+      onClick={() => signIn("google", { callbackUrl: "/" })}
       className="px-4 py-2 border flex gap-2 border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150"
     >
       <img className="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo"/>
@@ -17,7 +17,7 @@ export function LoginButton() {
 export function SignupButton() {
   return (
     <button
-      onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
+      onClick={() => signIn("google", { callbackUrl: "/" })}
       className="px-4 py-2 border flex gap-2 border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150"
     >
       <img className="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo"/>
